cd51f790cd099e5a1ca52ae584eb626a16eaff7e
Olga <olechka.mir28@mail.ru>
2023-06-16 07:34:53 +0300

    Example47

---
+++ Example47/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+//m = 3, n = 4.
+//0,5 7 -2 -0,2
+//1 -3,3 8 -9,9
+//8 7,8 -7,1 9
+using System;
+using static System.Console;
+Clear();
+double[,] GetArray(int m, int n)
+{
+    double[,] result = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            //result[i, j] = new Random().Next(100);
+            result[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+    }
+    return result;
+}
+void PrintArray(double[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j]} ");
+        }
+        WriteLine();
+    }
+}
+
+WriteLine("Введите число n: ");
+int n = int.Parse(ReadLine());
+WriteLine("Введите число m: ");
+int m = int.Parse(ReadLine());
+double[,] array = GetArray(m, n);
+PrintArray(array);
+WriteLine();
+
